5385e1ae678e50ff57c50213b98c6d39e3e8a6fb
William Martin <you@example.com>
2021-12-11 11:19:22 -0600

    Customized home page.

--- Pizzeria/settings.py
+++ Pizzeria/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'pizzas',
     'users',
     #third party apps
-    #'bootstrap4',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
